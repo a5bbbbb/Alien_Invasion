a3f7418438651c845b9527f6bcef436c78afb6fd
a5bbbbb <suprunoviktor@gmail.com>
2024-01-07 21:34:02 +0600

    Progress up to page 258.

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,8 @@ from ship import Ship
 
 from bullet import Bullet
 
+from alien import Alien
+
 class Alien_invasion:
 
     """Overall class to manage game assets and behavior."""
@@ -35,6 +37,10 @@ class Alien_invasion:
         self.ship = Ship(self)
 
         self.bullets = pygame.sprite.Group()
+
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
 
     def run_game(self):
@@ -137,6 +143,17 @@ class Alien_invasion:
                 
                 self.bullets.remove(bullet)
 
+    
+    def _create_fleet(self):
+
+        """Create the fleet of aliens."""
+
+        # Make an alien.
+
+        alien = Alien(self)
+
+        self.aliens.add(alien)
+
 
     def _update_screen(self):
 
@@ -151,6 +168,8 @@ class Alien_invasion:
             bullet.draw_bullet()
 
         self.ship.blitme()
+
+        self.aliens.draw(self.screen)
 
         # Make the most recently drawn screen visible.
                 
